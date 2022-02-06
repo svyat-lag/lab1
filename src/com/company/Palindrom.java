6e0cf8c5d6a99332d8a55859fed86bfd474ceb50
Svyatoslav Palibza <svyatpalibza.9@gmail.com>
2022-02-06 19:21:43 +0300

    Add the Primes & half of the Palindrom

---
+++ src/com/company/Palindrom.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Palindrom {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+    }
+}
---
+++ src/com/company/Primes.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import static java.lang.Math.sqrt;
+
+public class Primes {
+    public static void main(String[] args){
+        for (int i = 1; i <= 100; i++){
+            if (isPrime(i)) System.out.println(i);
+        }
+    }
+    public static boolean isPrime(int n){
+        for (int i = 2; i <= sqrt(n); i++){
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}

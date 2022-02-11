23e237d4aca8b3288a806e47c1696c251f04c587
Svyatoslav Palibza <svyatpalibza.9@gmail.com>
2022-02-11 09:51:23 +0300

    Fix some bugs

--- src/com/company/Palindrom.java
+++ src/com/company/Palindrom.java
@@ -1,22 +1,30 @@
 package com.company;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 
+// Determines whether user's string is a palindrom or not
 public class Palindrom {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        if (isPalindrom(s)) System.out.println("This string is a palindrom");
-        else System.out.println("This string is not a palindrom");
+        String usersString = scanner.nextLine();
+
+        try {
+            if (isPalindrom(usersString)) System.out.println("This string is a palindrom");
+            else System.out.println("This string is not a palindrom");
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 
-    public static boolean isPalindrom(String s){
-        // Transforms the given string to array
+    public static boolean isPalindrom(@NotNull String s){
+        final int MIDDLE_OF_THE_STRING = s.length()/2;
         char[] sArray = s.toCharArray();
 
         // Compares current symbol with the opposite one
-        // If the word has odd number of characters, then this function compare it with itself
-        for (int i = 0; i <= s.length()/2; i++){
+        // If the word has odd number of characters, then this function compare the middle character with itself *
+        for (int i = 0; i <= MIDDLE_OF_THE_STRING; i++){
             if (sArray[i] != sArray[s.length()-i-1]) return false;
         }
         return true;
--- src/com/company/Primes.java
+++ src/com/company/Primes.java
@@ -7,16 +7,27 @@ import static java.lang.Math.sqrt;
 public class Primes {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        try {
+            int usersNumber = scanner.nextInt();
 
-        for (int i = 1; i <= num; i++){
-            if (isPrime(i)) System.out.println(i);
+            // Checks if the user's number is negative or equal to zero
+            if (usersNumber > 0)
+                for (int i = 1; i <= usersNumber; i++) {
+                    if (isPrime(i)) System.out.println(i);
+                }
+            else
+                System.out.println("Your number can not be less than 1");
+
+        } catch (Exception e){
+            System.out.println("Your number must be integer");
         }
     }
 
-    public static boolean isPrime(int n){
-        for (int i = 2; i <= sqrt(n); i++){
-            if (n % i == 0) return false;
+    public static boolean isPrime(int number){
+        final double MAXIMUM_DIVISOR = sqrt(number);
+
+        for (int i = 2; i <= MAXIMUM_DIVISOR; i++){
+            if (number % i == 0) return false;
         }
         return true;
     }
